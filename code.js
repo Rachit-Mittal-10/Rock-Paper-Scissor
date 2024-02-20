05f8d8ae0c582929803d7f5de9cc5f8cd62d4ea0
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-02-21 02:56:02 +0530

    Basic UI added
    Functional Point Calculation
    Result String Display Functional

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
 "use strict";
 
+let playerPoint = 0;
+let computerPoint = 0;
+
 function getComputerChoice() {
     const elements = ["rock", "paper", "scissor"];
     const randomIndex = Math.floor(Math.random() * elements.length);
@@ -13,16 +16,29 @@ function recordPlayerSelection(button_val) {
     return playerChoice;
 }
 
+function handleResult(result){
+    const result_space = document.getElementsByClassName('result')[0];
+    const para_result = document.createElement('p');
+    para_result.textContent = result;
+    result_space.appendChild(para_result);
+    let playerPointDisplay = document.getElementById("player-points");
+    playerPointDisplay.textContent = playerPoint;
+    let computerPointDisplay = document.getElementById("computer-points");
+    computerPointDisplay.textContent = computerPoint;
+}
+
 function getPlayerSelection() {
     let choice_buttons = document.querySelectorAll(".choice-button");
-    let button;
     let playerChoice;
     let result;
     choice_buttons.forEach(function (button) {
         button.addEventListener('click', function () {
+            if(playerPoint==5 || computerPoint==5){
+                return;
+            }
             playerChoice = recordPlayerSelection(button.value);
             result = playRound(playerChoice);
-            return result;
+            handleResult(result);
         });
     });
 }
@@ -35,21 +51,29 @@ function playRound(playerChoice) {
         return "D";
     }
     else if(computerChoice === "rock" && playerChoice === "scissor"){
+        computerPoint++;
         return "L";
     }
     else if(computerChoice === "rock" && playerChoice === "paper"){
+        playerPoint++;
         return "W";
     }
     else if(computerChoice === "paper" && playerChoice === "scissor"){
+        playerPoint++;
         return "W";
     }
     else if(computerChoice === "paper" && playerChoice === "rock"){
+        computerPoint++;
         return "L";
     }
     else if(computerChoice === "scissor" && playerChoice === "paper"){
+        computerPoint++;
         return "L";
     }
     else if(computerChoice === "scissor" && playerChoice === "rock"){
+        playerPoint++;
         return "W";
     }
 }
+
+getPlayerSelection();
