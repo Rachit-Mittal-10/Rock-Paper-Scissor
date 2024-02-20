6b0268bb1a436a6ac53908319c7d8cb55fb5c899
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-02-21 00:45:58 +0530

    functionality for recording button value added

--- code.js
+++ code.js
@@ -1,33 +1,55 @@
 "use strict";
 
-function getComputerChoice(){
-    const elements = ["rock","paper","scissor"];
-    const randomIndex = Math.floor(Math.random()*elements.length);
+function getComputerChoice() {
+    const elements = ["rock", "paper", "scissor"];
+    const randomIndex = Math.floor(Math.random() * elements.length);
     const randomElementChosen = elements[randomIndex];
     return randomElementChosen;
 }
 
 
-function recordPlayerSelection(button_val){
+function recordPlayerSelection(button_val) {
     const playerChoice = document.getElementById(button_val).value;
     return playerChoice;
 }
 
-function getPlayerSelection(){
+function getPlayerSelection() {
     let choice_buttons = document.querySelectorAll(".choice-button");
     let button;
-    choice_buttons.forEach(function(button){
-        button.addEventListener('click',function(){
-            recordPlayerSelection(button.value);
+    let playerChoice;
+    let result;
+    choice_buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            playerChoice = recordPlayerSelection(button.value);
+            result = playRound(playerChoice);
+            return result;
         });
     });
 }
 
-function playGame(){
-    let i = 0;
-    while(i<5){
-        getPlayerSelection()
+
+// All result from player perspective
+function playRound(playerChoice) {
+    let computerChoice = getComputerChoice();
+    if(computerChoice === playerChoice){
+        return "D";
+    }
+    else if(computerChoice === "rock" && playerChoice === "scissor"){
+        return "L";
+    }
+    else if(computerChoice === "rock" && playerChoice === "paper"){
+        return "W";
+    }
+    else if(computerChoice === "paper" && playerChoice === "scissor"){
+        return "W";
+    }
+    else if(computerChoice === "paper" && playerChoice === "rock"){
+        return "L";
+    }
+    else if(computerChoice === "scissor" && playerChoice === "paper"){
+        return "L";
+    }
+    else if(computerChoice === "scissor" && playerChoice === "rock"){
+        return "W";
     }
 }
-
-playGame();
